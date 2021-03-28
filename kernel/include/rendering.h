544e9b15d0629d5f87eab2715232fdc14f466f28
antlilja <liljaanton2001@gmail.com>
2021-03-28 11:33:30 +0200

    Fix comments
    - Change comments to reflect name change in previous commit.
    - Make comments better reflect functionality.

--- kernel/include/rendering.h
+++ kernel/include/rendering.h
@@ -21,24 +21,27 @@ extern uint32_t g_bg_color;
 extern uint32_t g_fg_color;
 extern Framebuffer* g_frame_buffer;
 
+// Color pixel at pixel coordinates
 void put_pixel(uint64_t x, uint64_t y, uint32_t color);
+
+// Put character at character coordinates
 void put_char(char c, uint64_t x, uint64_t y, uint32_t fg, uint32_t bg);
 
-//print value in hex
-//returns the number of characters written (including 0x)
+// Put value in hex at character coordinates
+// returns the number of characters written (including 0x)
 uint64_t put_hex(uint64_t value, uint64_t x, uint64_t y);
 
-// print a certain amount of hex characters
+// Put exact amount of hex characters (nibbles) at character coordinates
 void put_hex_padleft(uint64_t value, uint64_t x, uint64_t y, uint64_t nibbles);
 
-// print 32 bits of an integer at character coordinates
+// Put 32 bits of an integer at character coordinates
 void put_hex_32(uint64_t value, uint64_t x, uint64_t y);
 
-// print 64 bits of an integer at character coordinates
+// Put 64 bits of an integer at character coordinates
 void put_hex_64(uint64_t value, uint64_t x, uint64_t y);
 
-//clear screen with a certain color
+// Clear screen with a certain color
 void clear_screen(uint32_t color);
 
-// print string and return length
+// Put string at character coordinates and return length
 uint64_t put_string(char* str, uint64_t x, uint64_t y);
