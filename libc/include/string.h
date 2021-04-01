2ad0ed3fc79a3038b3794188bd0ee236f643eeaf
Björn Thorén <bthoren@kth.se>
2021-04-01 12:53:58 +0200

    memcmp

--- libc/include/string.h
+++ libc/include/string.h
@@ -6,7 +6,11 @@ void *memchr(const void *str, int c, size_t n){
     //TODO
 }
 int memcmp(const void *str1, const void *str2, size_t n){
-    //TODO
+    char *c1 = (char*)str1;
+    char *c2 = (char*)str2;
+    for(size_t i = 0; i < n; ++i) {
+        if(c1[i] != c2[i]) return c1[i]-c2[i];
+    }
 }
 void *memcpy(void *dest, const void *src, size_t n){
     //TODO
