920a5d27f3875aa44ca8fcf2c8780b530d3e8a33
antlilja <liljaanton2001@gmail.com>
2021-04-01 19:52:25 +0200

    Use the naked attribute on functions in port_io.c
    This avoids the function prologue and epilogue which are totally
    unnecessary in these functions.

--- kernel/include/port_io.h
+++ kernel/include/port_io.h
@@ -1,20 +1,22 @@
 #pragma once
 #include <stdint.h>
 
+// The naked attribute is used to avoid the function prologue and epilogue
+
 // wrapper for outb
-void port_out_u8 (uint16_t addr, uint8_t data);
+__attribute__((naked)) void port_out_u8(uint16_t addr, uint8_t data);
 
 // wrapper for outw
-void port_out_u16 (uint16_t addr, uint16_t data);
+__attribute__((naked)) void port_out_u16(uint16_t addr, uint16_t data);
 
 // wrapper for outl
-void port_out_u32 (uint16_t addr, uint32_t data);
+__attribute__((naked)) void port_out_u32(uint16_t addr, uint32_t data);
 
 // wrapper for inb
-uint8_t port_in_u8 (uint16_t addr);
+__attribute__((naked)) uint8_t port_in_u8(uint16_t addr);
 
 // wrapper for inw
-uint16_t port_in_u16 (uint16_t addr);
+__attribute__((naked)) uint16_t port_in_u16(uint16_t addr);
 
 // wrapper for inl
-uint32_t port_in_u32 (uint16_t addr);
+__attribute__((naked)) uint32_t port_in_u32(uint16_t addr);
