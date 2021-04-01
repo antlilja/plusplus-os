25766f487ce2d82ec3759105f9c2c03209e13057
Björn Thorén <bthoren@kth.se>
2021-04-01 12:26:14 +0200

    Added all library functions to be defined in headers for stdlib.h and string.h

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -1,1 +1,88 @@
 #pragma once
+
+#define size_t unsigned
+
+double atof(const char *str){
+    //TODO
+}
+int atoi(const char *str){
+    //TODO
+}
+long int atol(const char *str){
+    //TODO
+}
+double strtod(const char *str, char **endptr){
+    //TODO
+}
+long int strtol(const char *str, char **endptr, int base){
+    //TODO
+}
+unsigned long int strtoul(const char *str, char **endptr, int base){
+    //TODO
+}
+void *calloc(size_t nitems, size_t size){
+    //TODO
+}
+void free(void *ptr){
+    //TODO
+}
+void *malloc(size_t size){
+    //TODO
+}
+void *realloc(void *ptr, size_t size){
+    //TODO
+}
+void abort(void){
+    //TODO
+}
+int atexit(void (*func)(void)){
+    //TODO
+}
+void exit(int status){
+    //TODO
+}
+char *getenv(const char *name){
+    //TODO
+}
+int system(const char *string){
+    //TODO
+}
+void *bsearch(const void *key, const void *base, size_t nitems, size_t size, int (*compar)(const void *, const void *)){
+    //TODO
+}
+void qsort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*)){
+    //TODO
+}
+int abs(int x){
+    //TODO
+}
+int div(int numer, int denom){
+    //TODO
+}
+long int labs(long int x){
+    //TODO
+}
+int ldiv(long int numer, long int denom){
+    //TODO
+}
+int rand(void){
+    //TODO
+}
+void srand(unsigned int seed){
+    //TODO
+}
+int mblen(const char *str, size_t n){
+    //TODO
+}
+size_t mbstowcs(int *pwcs, const char *str, size_t n){
+    //TODO
+}
+int mbtowc(int *pwc, const char *str, size_t n){
+    //TODO
+}
+size_t wcstombs(char *str, const wchar_t *pwcs, size_t n){
+    //TODO
+}
+int wctomb(char *str, wchar_t wchar){
+    //TODO
+}
--- libc/include/string.h
+++ libc/include/string.h
@@ -1,1 +1,70 @@
 #pragma once
+
+#define size_t unsigned
+
+void *memchr(const void *str, int c, size_t n){
+    //TODO
+}
+int memcmp(const void *str1, const void *str2, size_t n){
+    //TODO
+}
+void *memcpy(void *dest, const void *src, size_t n){
+    //TODO
+}
+void *memmove(void *dest, const void *src, size_t n){
+    //TODO
+}
+void *memset(void *str, int c, size_t n){
+    //TODO
+}
+char *strcat(char *dest, const char *src){
+    //TODO
+}
+char *strncat(char *dest, const char *src, size_t n){
+    //TODO
+}
+char *strchr(const char *str, int c){
+    //TODO
+}
+int strcmp(const char *str1, const char *str2){
+    //TODO
+}
+int strncmp(const char *str1, const char *str2, size_t n){
+    //TODO
+}
+int strcoll(const char *str1, const char *str2){
+    //TODO
+}
+char *strcpy(char *dest, const char *src){
+    //TODO
+}
+char *strncpy(char *dest, const char *src, size_t n){
+    //TODO
+}
+size_t strcspn(const char *str1, const char *str2){
+    //TODO
+}
+char *strerror(int errnum){
+    //TODO
+}
+size_t strlen(const char *str){
+    //TODO
+}
+char *strpbrk(const char *str1, const char *str2){
+    //TODO
+}
+char *strrchr(const char *str, int c){
+    //TODO
+}
+size_t strspn(const char *str1, const char *str2){
+    //TODO
+}
+char *strstr(const char *haystack, const char *needle){
+    //TODO
+}
+char *strtok(char *str, const char *delim){
+    //TODO
+}
+size_t strxfrm(char *dest, const char *src, size_t n){
+    //TODO
+}
