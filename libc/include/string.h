714fbe59f9a2d8339654a997589b9b1681400b6c
Björn Thorén <bthoren@kth.se>
2021-04-06 17:12:23 +0200

    Create basic string.h

---
+++ libc/include/string.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <stddef.h>
+
+int memcmp(const void *str1, const void *str2, size_t n);
+void *memcpy(void *dest, const void *src, size_t n);
+void *memmove(void *dest, const void *src, size_t n);
+void *memset(void *str, int c, size_t n);
+int strcmp(const char *str1, const char *str2);
---
+++ libc/src/string/memcmp.c
@@ -0,0 +1,10 @@
+#include <string.h>
+
+int memcmp(const void* str1, const void* str2, size_t n) {
+    const char* c1 = (const char*)str1;
+    const char* c2 = (const char*)str2;
+    for (size_t i = 0; i < n; ++i) {
+        if (c1[i] != c2[i]) return c1[i] - c2[i];
+    }
+    return 0;
+}
