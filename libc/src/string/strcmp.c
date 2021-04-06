eedd4519a90bd352cc97bdf108ba7b07646d7b6a
Björn Thorén <bthoren@kth.se>
2021-04-06 14:49:07 +0200

    strcmp efficency

--- libc/src/string/strcmp.c
+++ libc/src/string/strcmp.c
@@ -1,10 +1,8 @@
 #include <string.h>
 
 int strcmp(const char *str1, const char *str2){
-    char *c1 = (char*)str1;
-    char *c2 = (char*)str2;
-    for(size_t i = 0; c1[i] != '\0' && c2[i] != '\0'; ++i) {
-        if(c1[i] != c2[i]) return c1[i]-c2[i];
+    for(size_t i = 0; str1[i] != '\0' && str2[i] != '\0'; ++i) {
+        if(str1[i] != str2[i]) return (int)str1[i] - (int)str2[i];
     }
     return 0;
 }
