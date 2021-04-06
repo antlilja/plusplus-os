2e35c2eae50c6be321e931262bdc0365f508754c
Björn Thorén <bthoren@kth.se>
2021-04-06 14:03:53 +0200

    memchr declaration

--- libc/include/string.h
+++ libc/include/string.h
@@ -2,6 +2,7 @@
 
 #include <stddef.h>
 
+void *memchr(const void *str, int c, size_t n);
 int memcmp(const void *str1, const void *str2, size_t n);
 void *memcpy(void *dest, const void *src, size_t n);
 void *memmove(void *dest, const void *src, size_t n);
