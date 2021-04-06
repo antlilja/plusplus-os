8b0f41d8a3660bec2bfce643458f332466d1510f
Björn Thorén <bthoren@kth.se>
2021-04-06 14:38:29 +0200

    keep const

--- libc/src/string/memcpy.c
+++ libc/src/string/memcpy.c
@@ -1,7 +1,7 @@
 #include <string.h>
 
 void *memcpy(void *dest, const void *src, size_t n){
-    char *from = (char*)src;
+    const char *from = (char*)src;
     char *to = (char*)dest;
     for(size_t i = 0; i < n; ++i) to[i] = from[i];
     return dest;
