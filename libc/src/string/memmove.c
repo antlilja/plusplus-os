8cc722f365208d20173d5cb9f53eb6fb74378490
Björn Thorén <bthoren@kth.se>
2021-04-06 14:45:35 +0200

    memmove overlap compliency

--- libc/src/string/memmove.c
+++ libc/src/string/memmove.c
@@ -1,5 +1,10 @@
 #include <string.h>
 
 void *memmove(void *dest, const void *src, size_t n){
-    return memcpy(dest, src, n);
+    const char *from = (char*)src;
+    char *to = (char*)dest;
+    char data[n];
+    for(size_t i = 0; i < n; ++i) data[i] = from[i];
+    for(size_t i = 0; i < n; ++i) to[i]   = data[i];
+    return dest;
 }
