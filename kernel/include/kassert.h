7f9d85ca16f5d531e1a40893e3d1d81ebd49d040
antlilja <liljaanton2001@gmail.com>
2021-04-07 21:10:09 +0200

    Fix KERNEL_ASSERT with macro unlikely attribute

--- kernel/include/kassert.h
+++ kernel/include/kassert.h
@@ -3,8 +3,8 @@
 #ifdef ENABLE_KERNEL_ASSERTS
 #    include "rendering.h"
 #    define KERNEL_ASSERT(CONDITION, STR) \
-        if (!(CONDITION)) {               \
-            [[unlikely]] clear_screen(0); \
+        if (__builtin_expect(!(CONDITION), 0)) {	\
+            clear_screen(0);              \
             g_fg_color = 0xff0000;        \
             put_string(STR, 0, 0);        \
             while (1)                     \
