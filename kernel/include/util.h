658becaeb532ae03322f33564b7bd23b9a09dd82
antlilja <liljaanton2001@gmail.com>
2021-05-03 18:19:15 +0200

    Add bound_contains function to util
    This also makes range_contains use the bound_contains function as an
    implementation.

--- kernel/include/util.h
+++ kernel/include/util.h
@@ -23,3 +23,6 @@ uint64_t round_up_to_multiple(uint64_t n, uint64_t m);
 
 // Checks whether or not the value x is within the range [lower, lower + size)
 bool range_contains(uint64_t x, uint64_t lower, uint64_t size);
+
+// Checks whether or not the value x is within the range [lower, upper)
+bool bound_contains(uint64_t x, uint64_t lower, uint64_t upper);
--- kernel/src/util.c
+++ kernel/src/util.c
@@ -3,5 +3,7 @@
 uint64_t round_up_to_multiple(uint64_t n, uint64_t m) { return ((n + m - 1) / m) * m; }
 
 bool range_contains(uint64_t x, uint64_t lower, uint64_t size) {
-    return x >= lower && x < (lower + size);
+    return bound_contains(x, lower, lower + size);
 }
+
+bool bound_contains(uint64_t x, uint64_t lower, uint64_t upper) { return x >= lower && x < upper; }
