36680366db9a559ac2b9a48f1ae1b8b172725b8d
antlilja <liljaanton2001@gmail.com>
2021-05-03 18:18:25 +0200

    Add MAX and MIN macros to util.h

--- kernel/include/util.h
+++ kernel/include/util.h
@@ -2,6 +2,22 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+// Return maximum value of a and b
+#define MAX(a, b)             \
+    ({                        \
+        __auto_type _a = (a); \
+        __auto_type _b = (b); \
+        _a > _b ? _a : _b;    \
+    })
+
+// Return minimum value of a and b
+#define MIN(a, b)             \
+    ({                        \
+        __auto_type _a = (a); \
+        __auto_type _b = (b); \
+        _a < _b ? _a : _b;    \
+    })
+
 // Rounds n up to nearest multiple of m
 uint64_t round_up_to_multiple(uint64_t n, uint64_t m);
 
