402c8e9ce9cb154941502eed8ea56538d0455214
antlilja <liljaanton2001@gmail.com>
2021-05-12 13:51:55 +0200

    Cleanup gdt.c

--- kernel/src/gdt.c
+++ kernel/src/gdt.c
@@ -58,48 +58,43 @@ struct {
     uint16_t iopb_offset;
 } __attribute__((packed)) g_tss = {0};
 
-// Setup defines in assembly
-// Weird C nested macros are required here to concatenate
-// the inline assembly strings with the value of the macro as a string
+__attribute__((naked)) void set_gdt_and_tss(void* __attribute__((unused)) gdt) {
+    // Translate C defines into assembly "defines"
 #define STR(x) #x
 #define XSTR(s) STR(s)
-asm(".equ KERNEL_CODE_SEGMENT, " XSTR(GDT_KERNEL_CODE_SEGMENT) "\n");
-asm(".equ KERNEL_DATA_SEGMENT, " XSTR(GDT_KERNEL_DATA_SEGMENT) "\n");
-asm(".equ TSS_SEGMENT, " XSTR(GDT_TSS_SEGMENT) "\n");
+    asm(".equ KERNEL_CODE_SEGMENT, " XSTR(GDT_KERNEL_CODE_SEGMENT) "\n");
+    asm(".equ KERNEL_DATA_SEGMENT, " XSTR(GDT_KERNEL_DATA_SEGMENT) "\n");
+    asm(".equ TSS_SEGMENT, " XSTR(GDT_TSS_SEGMENT) "\n");
 #undef XSTR
 #undef STR
 
-// Define the function set_gdt in assembly
-asm("set_gdt_and_tss:\n"
+    asm(
+        // Disable interrupts before setting GDT and TSS
+        "cli\n"
 
-    // Disable interrupts before setting GDT and TSS
-    "cli\n"
+        // Set pointer to GDT
+        "lgdt (%rdi)\n"
 
-    // Set pointer to GDT
-    "lgdt (%rdi)\n"
+        // Setup offset to TSS
+        "mov $TSS_SEGMENT, %ax\n"
+        "ltr %ax\n"
 
-    // Setup offset to TSS
-    "mov $TSS_SEGMENT, %ax\n"
-    "ltr %ax\n"
+        // Set all segment registers to the kernel data segment
+        "mov $KERNEL_DATA_SEGMENT, %ax\n"
+        "mov %ax, %ds\n"
+        "mov %ax, %es\n"
+        "mov %ax, %fs\n"
+        "mov %ax, %gs\n"
+        "mov %ax, %ss\n"
 
-    // Set all segment registers to the kernel data segment
-    "mov $KERNEL_DATA_SEGMENT, %ax\n"
-    "mov %ax, %ds\n"
-    "mov %ax, %es\n"
-    "mov %ax, %fs\n"
-    "mov %ax, %gs\n"
-    "mov %ax, %ss\n"
-
-    // Push code segment onto stack before return address to use far return
-    // this sets the code segment register (cs)
-    "pop %rdi\n"
-    "mov $KERNEL_CODE_SEGMENT, %rax\n"
-    "push %rax\n"
-    "push %rdi\n"
-    "lretq\n");
-
-// This is the function defined in assembly
-extern void set_gdt_and_tss(void* gdt);
+        // Push code segment onto stack before return address to use far return
+        // this sets the code segment register (cs)
+        "pop %rdi\n"
+        "mov $KERNEL_CODE_SEGMENT, %rax\n"
+        "push %rax\n"
+        "push %rdi\n"
+        "lretq\n");
+}
 
 void setup_gdt_and_tss() {
     // Set io bitmap offset to the size of the TSS because we are not using it.
@@ -121,16 +116,13 @@ void setup_gdt_and_tss() {
     // We will give a pointer to this struct to the lgdt instruction
     struct {
         uint16_t size;
-        uint64_t base;
+        void* base;
     } __attribute__((packed)) gdt;
 
-    // TODO(Anton Lilja, 10-04-21):
-    // We have to do manual assignment here because the compiler might try to optimize in virtual
-    // address pointers at compile time otherwise.
-    // Find way to disable this optimization or
-    // make sure to map kernel correctly before any of this code.
+    // NOTE (Anton Lilja, 12/05/2021):
+    // For some reason we can't assign directly to the struct, it refuses to load the GDT if we do.
     gdt.size = sizeof(g_gdt) - 1;
-    gdt.base = (uint64_t)&g_gdt;
+    gdt.base = (void*)&g_gdt;
 
     set_gdt_and_tss((void*)&gdt);
 }
