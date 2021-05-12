3eb4a5553145761d4b02ae51d37aefd18289a97b
antlilja <liljaanton2001@gmail.com>
2021-05-12 13:58:41 +0200

    Simplify TSS struct

--- kernel/src/gdt.c
+++ kernel/src/gdt.c
@@ -40,20 +40,10 @@ struct {
 struct {
     uint32_t reserved0;
     // Stack pointers for different privilege levels
-    uint64_t rsp0;
-    uint64_t rsp1;
-    uint64_t rsp2;
+    void* rsp[3];
     uint64_t reserved1;
-    // Interrupt stack table
-    uint64_t ist1;
-    uint64_t ist2;
-    uint64_t ist3;
-    uint64_t ist4;
-    uint64_t ist5;
-    uint64_t ist6;
-    uint64_t ist7;
-    uint64_t reserved2;
-    uint16_t reserved3;
+    void* interrupt_stack_table[7];
+    uint8_t reserved2[10];
     // IO bitmap
     uint16_t iopb_offset;
 } __attribute__((packed)) g_tss = {0};
