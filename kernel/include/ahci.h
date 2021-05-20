f67e1ff9529a3897310c2ff3eb913aca98788841
antlilja <liljaanton2001@gmail.com>
2021-05-20 22:12:30 +0200

    Added AHCI read_write_sectors func

--- kernel/include/ahci.h
+++ kernel/include/ahci.h
@@ -2,4 +2,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+void read_write_sectors(uint8_t device_id, uint64_t sector, uint16_t sector_count, void* buffer,
+                        bool write);
+
 void initialize_ahci();
